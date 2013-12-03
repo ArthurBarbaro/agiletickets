94d4a6a5d3962a99669048e1ed79e989fcb3f518
GECOS <agil4037@caelum-20120927-3.(none)>
2013-12-03 12:50:19 -0200

    refactor PreencheBanco

--- src/main/java/br/com/caelum/agiletickets/PreencheBanco.java
+++ src/main/java/br/com/caelum/agiletickets/PreencheBanco.java
@@ -17,37 +17,64 @@ public class PreencheBanco {
 	public static void main(String[] args) {
 		EntityManagerFactoryCreator creator = new EntityManagerFactoryCreator();
 		creator.create();
-		EntityManagerCreator managerCreator = new EntityManagerCreator(creator.getInstance());
+		EntityManagerCreator managerCreator = new EntityManagerCreator(
+				creator.getInstance());
 		managerCreator.create();
 		EntityManager manager = managerCreator.getInstance();
 
 		manager.getTransaction().begin();
-		manager.createQuery("delete from Sessao").executeUpdate();
-		manager.createQuery("delete from Espetaculo").executeUpdate();
-		manager.createQuery("delete from Estabelecimento").executeUpdate();
-		Estabelecimento estabelecimento = new Estabelecimento();
-		estabelecimento.setNome("Casa de shows");
-		estabelecimento.setEndereco("Rua dos Silveiras, 12345");
 
-		Espetaculo espetaculo = new Espetaculo();
-		espetaculo.setEstabelecimento(estabelecimento);
-		espetaculo.setNome("Depeche Mode");
-		espetaculo.setTipo(TipoDeEspetaculo.SHOW);
+		executaQuery("delete from Sessao", manager);
+		executaQuery("delete from Espetaculo", manager);
+		executaQuery("delete from Estabelecimento", manager);
+		Estabelecimento estabelecimento = criaEstabelecimento("Casa de shows",
+				"Rua dos Silveiras, 12345");
+
+		Espetaculo espetaculo = criaEspetaculo(estabelecimento, "Depeche Mode",
+				TipoDeEspetaculo.SHOW);
 
 		manager.persist(estabelecimento);
 		manager.persist(espetaculo);
 
 		for (int i = 0; i < 10; i++) {
-			Sessao sessao = new Sessao();
-			sessao.setEspetaculo(espetaculo);
-			sessao.setInicio(new DateTime().plusDays(7+i));
-			sessao.setDuracaoEmMinutos(60 * 3);
-			sessao.setTotalIngressos(10);
-			sessao.setIngressosReservados(10 - i);
+			Sessao sessao = criaSessao(espetaculo,
+					new DateTime().plusDays(7 + i), 60 * 3, 10, 10 - i);
 			manager.persist(sessao);
 		}
 
 		manager.getTransaction().commit();
 		manager.close();
+	}
+
+	private static Sessao criaSessao(Espetaculo espetaculo, DateTime inicio,
+			Integer duracao, Integer totalIngressos, Integer ingressosReservados) {
+		Sessao sessao = new Sessao();
+		sessao.setEspetaculo(espetaculo);
+		sessao.setInicio(inicio);
+		sessao.setDuracaoEmMinutos(duracao);
+		sessao.setTotalIngressos(totalIngressos);
+		sessao.setIngressosReservados(ingressosReservados);
+		return sessao;
+	}
+
+	private static Espetaculo criaEspetaculo(Estabelecimento estabelecimento,
+			String nome, TipoDeEspetaculo tipo) {
+		Espetaculo espetaculo = new Espetaculo();
+		espetaculo.setEstabelecimento(estabelecimento);
+		espetaculo.setNome(nome);
+		espetaculo.setTipo(tipo);
+		return espetaculo;
+	}
+
+	private static Estabelecimento criaEstabelecimento(String nome,
+			String endereco) {
+		Estabelecimento estabelecimento = new Estabelecimento();
+		estabelecimento.setNome(nome);
+		estabelecimento.setEndereco(endereco);
+		return estabelecimento;
+	}
+
+	private static int executaQuery(String sql, EntityManager manager) {
+		return manager.createQuery(sql).executeUpdate();
 	}
 }
